eb5f76ed81fc59e26a070cbde44acf51e0e4ab75
邱阿朋 <qiuapeng921@163.com>
2020-08-27 16:26:37 +0800

    update

--- routers/admin.go
+++ routers/admin.go
@@ -8,16 +8,16 @@ import (
 
 // 后台路由组
 func InitAdminRouter(router *gin.Engine) {
-	router.GET("/admin/login", admin.Login)
+	router.POST("/admin/login", admin.Login)
 	adminGroup := router.Group("/admin")
 	{
 		// 权限验证中间件
 		adminGroup.Use(middleware.AdminAuth())
-		adminGroup.GET("/detail", admin.Login)
+		adminGroup.POST("/detail", admin.Login)
 		roleGroup := adminGroup.Group("/role")
 		{
-			roleGroup.GET("/list", admin.RoleList)
-			roleGroup.GET("/detail", admin.RoleDetail)
+			roleGroup.POST("/list", admin.RoleList)
+			roleGroup.POST("/detail", admin.RoleDetail)
 			roleGroup.POST("/create", admin.RoleCreate)
 			roleGroup.POST("/update", admin.RoleUpdate)
 			roleGroup.POST("/delete", admin.RoleDelete)
