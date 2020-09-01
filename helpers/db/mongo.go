6ab71b8c70f2bbacc56b51f8fe6469a5a26ed4ad
邱阿朋 <qiuapeng921@163.com>
2020-09-01 19:01:46 +0800

    多模板继承

--- helpers/db/mongo.go
+++ helpers/db/mongo.go
@@ -20,21 +20,21 @@ func InitMongo() {
 	host := os.Getenv("MONGO_HOST")
 	user := os.Getenv("MONGO_USERNAME")
 	pass := os.Getenv("MONGO_PASSWORD")
-	uri := fmt.Sprintf("mongodb://%s:%s@%s/admin", user, pass, host)
+	database := os.Getenv("MONGO_DATABASE")
+	uri := fmt.Sprintf("mongodb://%s:%s@%s/%s", user, pass, host, database)
 
 	maxPool, _ := strconv.ParseUint(os.Getenv("MONGO_MAX_POOL"), 10, 64)
 	var err error
 	// 连接数据库
 	mongoClient, err = mongo.Connect(ctx, options.Client().ApplyURI(uri).SetMaxPoolSize(maxPool))
 	if err != nil {
-		panic(fmt.Sprintf("Mongo初始化失败 错误信息：%s",err.Error()))
+		panic(fmt.Sprintf("Mongo初始化失败 错误信息：%s", err.Error()))
 	}
 	// 判断服务是不是可用
 	err = mongoClient.Ping(context.TODO(), nil)
 	if err != nil {
 		panic(err.Error())
 	}
-	database := os.Getenv("MONGO_DATABASE")
 	mongoClient.Database(database)
 	fmt.Println("mongodb连接成功")
 }
--- helpers/templates/templates.go
+++ helpers/templates/templates.go
@@ -16,21 +16,39 @@ func InitTemplate(router *gin.Engine) {
 func loadTemplates(templatesDir string) multitemplate.Renderer {
 	renderer := multitemplate.NewRenderer()
 
-	layouts, err := filepath.Glob(templatesDir + "/layouts/*.html")
+	// 加载前台模板
+	frontendLayouts, err := filepath.Glob(templatesDir + "/layouts/frontend_base.html")
 	if err != nil {
 		panic(err.Error())
 	}
-
-	pages, pagesError := filepath.Glob(templatesDir + "/pages/*.html")
+	frontendPages, pagesError := filepath.Glob(templatesDir + "/frontend/*.html")
 	if pagesError != nil {
 		panic(pagesError.Error())
 	}
 
-	for _, page := range pages {
-		layoutCopy := make([]string, len(layouts))
-		copy(layoutCopy, layouts)
+	for _, page := range frontendPages {
+		layoutCopy := make([]string, len(frontendLayouts))
+		copy(layoutCopy, frontendLayouts)
 		files := append(layoutCopy, page)
 		renderer.AddFromFilesFuncs(filepath.Base(page), FuncMap(), files...)
 	}
+
+	// 加载后台模板
+	backendLayouts, err := filepath.Glob(templatesDir + "/layouts/backend_base.html")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	backendPages, err := filepath.Glob(templatesDir + "/backend/*.html")
+	if err != nil {
+		panic(err.Error())
+	}
+	for _, page := range backendPages {
+		layoutCopy := make([]string, len(backendLayouts))
+		copy(layoutCopy, backendLayouts)
+		files := append(layoutCopy, page)
+		renderer.AddFromFilesFuncs(filepath.Base(page), FuncMap(), files...)
+	}
+
 	return renderer
 }
