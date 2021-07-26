7bb902effd85530c7a2513d668bcad78c7414859
qiuapeng921 <qiuapeng@vchangyi.com>
2021-07-26 10:12:58 +0800

    修改转换函数

--- app/utility/system/convert.go
+++ app/utility/system/convert.go
@@ -2,32 +2,26 @@ package system
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/goinggo/mapstructure"
 	"reflect"
 )
 
-// JsonToStruct
 func JsonToStruct(jsonStr string, obj interface{}) error {
 	err := json.Unmarshal([]byte(jsonStr), &obj)
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
 	return nil
 }
 
-// struct转json
-func StructToJson(obj interface{})string {
+func StructToJson(obj interface{}) string {
 	jsonBytes, err := json.Marshal(obj)
 	if err != nil {
-		fmt.Println(err)
 		return ""
 	}
 	return string(jsonBytes)
 }
 
-// json转map
 func JsonToMap(jsonStr string) (result map[string]interface{}) {
 	err := json.Unmarshal([]byte(jsonStr), &result)
 	if err != nil {
@@ -36,25 +30,22 @@ func JsonToMap(jsonStr string) (result map[string]interface{}) {
 	return result
 }
 
-// map转json
 func MapToJson(instance map[string]interface{}) string {
 	jsonStr, err := json.Marshal(instance)
 	if err != nil {
-		fmt.Println("MapToJsonDemo err: ", err)
+		panic(err)
 	}
 	return string(jsonStr)
 }
 
-// map转struct
 func MapToStruct(instance map[string]interface{}, people struct{}) struct{} {
 	err := mapstructure.Decode(instance, &people)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 	return people
 }
 
-// struct转map
 func StructToMap(obj interface{}) map[string]interface{} {
 	objType := reflect.TypeOf(obj)
 	objValue := reflect.ValueOf(obj)
